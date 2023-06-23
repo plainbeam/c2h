a555013006837ac1a67ba335746b98cea508a967
Benn Pelen <pelendev@gmail.com>
2023-06-24 06:33:32 +0800

    init

---
+++ C2H/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C2H.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
